2913bfe942117b8c1a03e67084b199d6dae02ea6
jerry <jerry@kiwigo.tw>
2024-09-19 14:59:19 +0800

    first commit

---
+++ src/view/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home-page">
+    <div class="_header">
+      <img src="@/assets/logo.png" />
+    </div>
+    <div class="_title">
+        <img src="@/assets/title.png" />
+    </div>
+    <span class="_name">Ellis．業務經理</span>
+    
+    <div class="_content">
+        <div class="_item phone">
+            <span>聯繫電話</span>
+            <span>0912-624625</span>
+        </div>
+        <div class="_item email">
+            <span>電子信箱</span>
+            <span>service@kiwigo.tw</span>
+        </div>
+        <div class="_item company-phone">
+            <span>公司電話</span>
+            <span>04-22542663</span>
+        </div>
+        <div class="_item company-name">
+            <span>公司名稱</span>
+            <span>果亞創意設計公司</span>
+        </div>
+        <div class="_item company-id">
+            <span>統一編號</span>
+            <span>90642791</span>
+        </div>
+    </div>
+      
+
+  </div>
+</template>
+
+<script>
+// 引入圖片檔案
+export default {
+  name: "HomePage",
+};
+</script>
